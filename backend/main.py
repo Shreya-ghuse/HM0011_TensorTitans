c6d33d47ad1607a00c95a233217a5538bd6fbd3d
Shreya Ghuse <shreyaghuse@gmail.com>
2025-03-02 11:27:10 +0530

    Updated backend code

--- backend/main.py
+++ backend/main.py
@@ -2,5 +2,7 @@ from app import create_app, db
 
 app = create_app()
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=5000)
+
